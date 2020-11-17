93c002aabb60dbedee683170854ae72d59d9a531
Natalia Moreno <natlcs20@icloud.com>
2020-11-17 15:03:21 -0500

    We have a rough user interface but it works

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // The guess made by user
+    @State private var theUsersGuess = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            
+            Text("Guessing game")
+            Text("I'm thinking on a number between 1 and 100. Guess what it is!")
+            
+            TextField("Enter youre guess here", text: $theUsersGuess)
+            
+            Button("Submit Guess") {
+                // Check the guess
+            }
+            Text("You guessed \(theUsersGuess)")
+        }
     }
 }
 
